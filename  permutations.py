0315a1ceab50e07f450187abde39ecad8fce78ae
sspchunduri <sspchunduri@gmail.com>
2020-04-16 19:12:47 +0530

    Permutations.py

---
+++  permutations.py
@@ -0,0 +1,14 @@
+from itertools import permutations as per
+def permutations(string):
+    return set([''.join(p) for p in per(string)])
+
+"""
+In this kata you have to create all permutations of an input string and remove duplicates, if present. This means, you have to shuffle all letters from the input in all possible orders.
+
+Examples:
+
+permutations('a'); # ['a']
+permutations('ab'); # ['ab', 'ba']
+permutations('aabb'); # ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+The order of the permutations doesn't matter.
+"""
